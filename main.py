8db768e7bbef8ce76c424fa802180c604aa2503b
AlexanderAbt <147716720+AlexanderAbt@users.noreply.github.com>
2026-03-09 12:00:29 +0100

    feat: add option to use ollama instead of openai

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 import os
 import fitz
-import openai
 from openai import OpenAI
 
 def get_API_Key():
@@ -19,24 +18,43 @@ def read_file(file_path):
             text += page.get_text()
     return text
 
-def ask_question(text, question):
-    client = OpenAI()
-    response = client.responses.create(
-    model="gpt-4o-mini",
-    input= text + question 
-)
-    return response
-
-
 def parse_output(response):
     answer = response.output[0].content[0].text
     return answer
 
+def ask_question(text, question, provider = "OpenAI"):
+    if provider == "Ollama":
+        client = OpenAI(
+        base_url='http://localhost:11434/v1/',
+        api_key='ollama',  # required but ignored
+)
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    'role': 'user',
+                    'content': text + question,
+                }
+            ],
+        model='llama3.2',
+)
+        return response.choices[0].message.content
+
+    else: 
+        client = OpenAI()
+        response = client.responses.create(
+            model="gpt-4o-mini",
+            input= text + question 
+)
+
+        return parse_output(response)
+
 if __name__ == "__main__":
     get_API_Key()
+    provider_choice = input("Please select provider (OpenAI or Ollama)")
     response = ask_question(
         read_file("test_document.pdf"),
-        "Summarize the content of the following document"
+        "Summarize the content of the following document",
+        provider_choice
 )
-    answer = parse_output(response)
-    print(answer)
+    print(response)
+    
