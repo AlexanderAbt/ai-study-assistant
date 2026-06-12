5294b8e46c63ce212e114a27079330f86a5792e0
AlexanderAbt <147716720+AlexanderAbt@users.noreply.github.com>
2026-06-12 12:55:20 +0200

    chore: improved stability with different file names

--- retrieval/vector_store.py
+++ retrieval/vector_store.py
@@ -4,11 +4,20 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 import os
 import chromadb
 import logging
+import re
+import unicodedata
 
 logger = logging.getLogger(__name__)
 
+def santize_name(name : str) -> str: 
+    name = unicodedata.normalize("NFKD", name)
+    name = name.encode("ascii", "ignore").decode("ascii")
+    name = re.sub(r"[^a-zA-Z0-9._-]", "_", name)
+    name = name.strip("._-")
+    return name
 
 def get_collection(file_path: str) -> chromadb.Collection:
+    collection_name = santize_name(file_path)
     logger.info(f"Creating or getting collection for {file_path}")
     client = chromadb.PersistentClient(path = "chroma_db/")
-    return client.get_or_create_collection(file_path)
+    return client.get_or_create_collection(collection_name)
